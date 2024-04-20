17226e7a61e5c0ad6fbbd33048888d931c64e7d1
Olga Semenova <helen597597@gmail.com>
2024-04-20 20:15:57 +0500

    create generics

--- studying/urls.py
+++ studying/urls.py
@@ -1,6 +1,8 @@
+from django.urls import path
+
 from studying.apps import StudyingConfig
 from rest_framework.routers import DefaultRouter
-from studying.views import CourseViewSet
+from studying.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = StudyingConfig.name
 
@@ -8,5 +10,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
+    path('lessons/create/', LessonCreateAPIView.as_view(), name='lessons-create'),
+    path('lessons/', LessonListAPIView.as_view(), name='lessons-list'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lessons-get'),
+    path('lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lessons-update'),
+    path('lessons/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lessons-delete'),
 ] + router.urls
