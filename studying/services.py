fca8355c4818686ac37147707eeb36e5e5f1fdaf
Olga Semenova <helen597597@gmail.com>
2024-05-08 02:56:00 +0500

    add stripe

--- studying/services.py
+++ studying/services.py
@@ -8,16 +8,16 @@ stripe.api_key = STRIPE_API_KEY
 def create_product(product):
     """Create stripe product"""
     stripe_product = stripe.Product.create(name=product.title)
-    return stripe_product.id
+    return stripe_product.get("id")
 
 
 def create_price(amount):
     """Create stripe price"""
     stripe_price = stripe.Price.create(product.price, currency='rub', amount=amount * 100, product_data={"name": "Payment"})
-    return stripe_price.id
+    return stripe_price.get("id")
 
 
 def create_session(price):
     """Create stripe session"""
-    stripe_session = stripe.checkout.Session.create(success_url="", line_items=price, mode="payment")
-    return stripe_session.url
+    stripe_session = stripe.checkout.Session.create(success_url="http://127.0.0.1:8000", line_items=[{"price": price, "quantity": 1}], mode="payment")
+    return stripe_session.get("id"), stripe_session.get("url")
