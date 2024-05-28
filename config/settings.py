861dd9f68fbbc8226bda82b3a12fd7e1b542adc1
Olga Semenova <helen597597@gmail.com>
2024-05-29 02:48:58 +0500

    add docker-compose.yaml

--- config/settings.py
+++ config/settings.py
@@ -199,10 +199,10 @@ STRIPE_API_KEY = os.getenv('STRIPE_API_KEY ')
 # Настройки для Celery
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis, который по умолчанию работает на порту 6379
+CELERY_BROKER_URL = 'redis://redis:6379'  # Например, Redis, который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = "Asia/Yekaterinburg"
