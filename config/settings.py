873b5aadb96d0c6286e6fc2cdd0504f7dd4e3aba
Olga Semenova <helen597597@gmail.com>
2024-05-29 01:28:43 +0500

    add Dockerfile

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),  # Название БД
-        'USER': os.getenv('DB_USER'),  # Пользователь для подключения
-        'PASSWORD': os.getenv('DB_PASSWORD'),  # Пароль для этого пользователя
-        'HOST': os.getenv('POSTGRES_HOST'),  # Адрес, на котором развернут сервер БД
-        'PORT': os.getenv('POSTGRES_PORT'),  # Порт, на котором работает сервер БД
+        'NAME': os.getenv('NAME'),  # Название БД
+        'USER': os.getenv('POSTGRES_USER'),  # Пользователь для подключения
+        'PASSWORD': os.getenv('PASSWORD'),  # Пароль для этого пользователя
+        'HOST': os.getenv('HOST'),  # Адрес, на котором развернут сервер БД
+        'PORT': os.getenv('PORT'),  # Порт, на котором работает сервер БД
     }
 }
 
